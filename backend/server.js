b19dcd3d0bc1019b5cc1e5e30a6e7600f06ab42d
techwizard31 <istaprasad.patra@gmail.com>
2024-01-09 03:37:33 +0530

    8

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutroutes = require("./routes/workouts.js");
 const userRoutes = require("./routes/user.js");
-
+const PORT = process.env.PORT || 4000
 const app = express();
 
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use("/api/user", userRoutes);
 mongoose
   .connect(process.env.MONG_URI)
   .then(() => {
-    app.listen( process.env.PORT, () => {
+    app.listen(PORT, () => {
       console.log("connected to mongodb and listening at port");
     });
   })
